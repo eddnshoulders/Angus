bf8f0c48dd9e974f7ed0ee0b81ed54630b3266ef
eddnshoulders <eddnshoulders@gmail.com>
2026-05-27 15:18:45 +0100

    enc_input: rename outputs to match ang_sel interface (ab_enc, z_enc, ab_period_enc, ppr_enc, ab_count_enc)

--- hdl/enc_input.vhd
+++ hdl/enc_input.vhd
@@ -8,6 +8,9 @@ use ieee.numeric_std.all;
 -- Placeholder for encoder A/B/Z input path.
 -- Outputs tied to safe defaults. Will be implemented when encoder
 -- hardware is available.
+--
+-- Port names match ang_sel expectations:
+--   ab_enc, z_enc, ab_period_enc, ppr_enc, ab_count_enc, signal_present
 -- =============================================================================
 
 entity enc_input is
@@ -21,10 +24,11 @@ entity enc_input is
         z_edge_sel     : in  std_logic;
         n_teeth        : in  unsigned(7 downto 0);
         n_pulses       : in  unsigned(7 downto 0);
-        ab             : out std_logic;
-        z              : out std_logic;
-        tooth_period   : out unsigned(31 downto 0);
-        tooth_count    : out unsigned(7 downto 0);
+        ab_enc         : out std_logic;
+        z_enc          : out std_logic;
+        ab_period_enc  : out unsigned(31 downto 0);
+        ppr_enc        : out unsigned(7 downto 0);
+        ab_count_enc   : out unsigned(7 downto 0);
         signal_present : out std_logic;
         enc_edge_pulse : out std_logic
     );
@@ -33,10 +37,11 @@ end entity enc_input;
 architecture rtl of enc_input is
 begin
     -- Stub: all outputs safe defaults
-    ab             <= '0';
-    z              <= '0';
-    tooth_period   <= (others => '0');
-    tooth_count    <= (others => '0');
+    ab_enc         <= '0';
+    z_enc          <= '0';
+    ab_period_enc  <= (others => '0');
+    ppr_enc        <= n_teeth;  -- pass through so ang_sel has a valid ppr
+    ab_count_enc   <= (others => '0');
     signal_present <= '0';
     enc_edge_pulse <= '0';
 end architecture rtl;
